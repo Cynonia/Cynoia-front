ff0a7b098b77b187608f9732edfa4dc922ebf8bb
madymanage <mady.sankh@gmail.com>
2025-09-14 06:19:35 +0000

    Intégration de la page membres

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,5 +2,7 @@ export * from './auth.service';
 export * from './api.service';
 export * from './branding.service';
 export * from './spaces.service';
+export * from './reservations.service';
+export * from './members.service';
 // export * from './toast.service';
 // export * from './socket.service';
